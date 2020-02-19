66bbfbc3def080b5d854d90c77e1d778b4fcde7e
nirc7 <i2cs2013@gmail.com>
2020-02-19 14:03:28 +0200

    second change

--- Try1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Try1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world!");
+            Console.WriteLine("welcome to our course!");
         }
     }
 }
