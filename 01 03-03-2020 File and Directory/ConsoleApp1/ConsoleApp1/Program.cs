2d98df23e1bb5295b9e86cb70e9627a3d56d35c7
Lab1 RUPPIN <lab1@ruppin.ac.il>
2020-03-03 13:01:02 +0200

    added file and dir demo

---
+++ 01 03-03-2020 File and Directory/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("file.txt");
+            string str = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
+        }
+    }
+}
