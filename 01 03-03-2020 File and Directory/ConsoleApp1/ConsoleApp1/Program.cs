f7aef9ea6db7bc62ada68c137393b1f58d3d7e6f
30-400 <30-400@LAB-30-400>
2020-03-04 09:25:50 +0200

    more files..

--- 01 03-03-2020 File and Directory/ConsoleApp1/ConsoleApp1/Program.cs
+++ 01 03-03-2020 File and Directory/ConsoleApp1/ConsoleApp1/Program.cs
@@ -18,6 +18,44 @@ namespace ConsoleApp1
                 Console.WriteLine(sr.ReadLine());
             }
             sr.Close();
+            Console.WriteLine();
+            sr = new StreamReader("file.txt");
+            str = sr.ReadToEnd();
+            Console.WriteLine(str);
+            sr.Close();
+
+            //str.Replace(" ", "");
+            Console.WriteLine("________________");
+            string[] lines = str.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);// "\r\n"
+            foreach (var item in lines)
+            {
+                Console.WriteLine(item);
+            }
+
+            StreamWriter sw = new StreamWriter("file.txt");
+            sw.WriteLine("avi");
+            sw.WriteLine("benny");
+            sw.Close();
+
+
+            string filePath = Directory.GetCurrentDirectory();
+            Console.WriteLine(filePath);
+            FileInfo fi = new FileInfo("file.txt");
+            Console.WriteLine(fi.Extension);
+            Console.WriteLine(fi.CreationTime);
+            Console.WriteLine(fi.Name );
+
+            File.AppendAllText("file.txt","charlie");
+            //Directory.
+
+
+
         }
+    }
+
+    class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
     }
 }
