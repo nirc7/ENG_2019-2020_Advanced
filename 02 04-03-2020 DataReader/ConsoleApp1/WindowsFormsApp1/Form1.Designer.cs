e73e73c6dd16a5fbe53a9526f5e7ab2aa6b570a0
30-400 <30-400@LAB-30-400>
2020-03-04 14:39:12 +0200

    DB ex

---
+++ 02 04-03-2020 DataReader/ConsoleApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtFamily = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTable = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(100, 146);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "SELECT";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(99, 54);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtFamily
+            // 
+            this.txtFamily.Location = new System.Drawing.Point(100, 109);
+            this.txtFamily.Name = "txtFamily";
+            this.txtFamily.Size = new System.Drawing.Size(100, 20);
+            this.txtFamily.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "FAMILY";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(99, 80);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "NAME";
+            // 
+            // lblTable
+            // 
+            this.lblTable.AutoSize = true;
+            this.lblTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblTable.Location = new System.Drawing.Point(99, 246);
+            this.lblTable.Name = "lblTable";
+            this.lblTable.Size = new System.Drawing.Size(21, 20);
+            this.lblTable.TabIndex = 7;
+            this.lblTable.Text = "...";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(102, 175);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 8;
+            this.btnInsert.Text = "INSERT";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(195, 175);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(291, 175);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 693);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.lblTable);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFamily);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtFamily;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTable;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ 02 04-03-2020 DataReader/ConsoleApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string conStr = @"Data Source=LAB-30-400;Initial Catalog=UsersDB;Integrated Security=True";
+        SqlConnection con = null;
+        SqlCommand comm = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            con = new SqlConnection(conStr);
+            comm = new SqlCommand();
+            comm.Connection = con;
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            UpdateTable();
+        }
+
+        private void UpdateTable()
+        {
+            lblTable.Text = ExecuteQ(
+                            " SELECT * " +
+                            " FROM TBUsers " +
+                            " ORDER BY NAME ");
+        }
+
+        private string ExecuteQ(string command)
+        {
+            string output = "";
+            comm.CommandText = command;
+            con.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                output += reader["ID"] + " , " + reader["Name"].ToString() + " , " + reader[2] + "\n";
+            }
+            con.Close();
+            return output;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            int res = ExecuteNonQ(
+                $" INSERT INTO TBUsers(Name, Family)" +
+                 " VALUES('" + txtName.Text + $"','{txtFamily.Text}') ");
+        }
+
+        private int ExecuteNonQ(string command)
+        {
+            comm.CommandText = command;
+            con.Open();
+            int res = comm.ExecuteNonQuery();
+            con.Close();
+
+            UpdateTable();
+
+            return res;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int res = ExecuteNonQ(
+                  $" UPDATE TBUsers Set Name='{txtName.Text}', Family='{txtFamily.Text}' " +
+                  $" WHERE Id={txtId.Text}");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int res = ExecuteNonQ(
+                  $" DELETE TBUsers " +
+                  $" WHERE Id={txtId.Text}");
+        }
+    }
+}
